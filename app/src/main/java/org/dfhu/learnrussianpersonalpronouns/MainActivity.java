9d5353c0b167815388a872308a03bd504978199b
Victory <git@dfhu.org>
2015-08-11 19:39:27 -0400

    Toolbar colors but wrong place

--- app/src/main/java/org/dfhu/learnrussianpersonalpronouns/MainActivity.java
+++ app/src/main/java/org/dfhu/learnrussianpersonalpronouns/MainActivity.java
@@ -2,8 +2,10 @@ package org.dfhu.learnrussianpersonalpronouns;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 
     @Override
