3c9c8d0f7ae06fe60282b277816d3862c29e96fa
Victory <git@dfhu.org>
2015-08-11 20:07:03 -0400

    Some toolbar (ActionBar) styling.

--- app/src/main/java/org/dfhu/learnrussianpersonalpronouns/MainActivity.java
+++ app/src/main/java/org/dfhu/learnrussianpersonalpronouns/MainActivity.java
@@ -9,12 +9,14 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
     }
 
