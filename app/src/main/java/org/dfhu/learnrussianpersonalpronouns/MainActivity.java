7afa8965e82db164190b8c3c66389e788ff6483e
Victory <git@dfhu.org>
2015-08-12 19:48:31 -0400

    Some more toolbar styling.

--- app/src/main/java/org/dfhu/learnrussianpersonalpronouns/MainActivity.java
+++ app/src/main/java/org/dfhu/learnrussianpersonalpronouns/MainActivity.java
@@ -1,5 +1,6 @@
 package org.dfhu.learnrussianpersonalpronouns;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -18,6 +19,12 @@ public class MainActivity extends AppCompatActivity {
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        final ActionBar ab = getSupportActionBar();
+        if (ab != null) {
+            ab.setHomeAsUpIndicator(R.mipmap.ic_reorder_24dp);
+            ab.setDisplayHomeAsUpEnabled(true);
+        }
     }
 
     @Override
