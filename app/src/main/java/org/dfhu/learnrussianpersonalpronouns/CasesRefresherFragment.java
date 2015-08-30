2dfb72a8a492a86898f77a0c28e912429087f351
Victory <git@dfhu.org>
2015-08-30 12:18:44 -0400

    Show a stub dialog for nominative case

--- app/src/main/java/org/dfhu/learnrussianpersonalpronouns/CasesRefresherFragment.java
+++ app/src/main/java/org/dfhu/learnrussianpersonalpronouns/CasesRefresherFragment.java
@@ -1,22 +1,49 @@
 package org.dfhu.learnrussianpersonalpronouns;
 
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class CasesRefresherFragment extends Fragment {
+public class CasesRefresherFragment extends Fragment implements View.OnClickListener {
+    private View theView;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.cases_referesher_fragment, container, false);
+
+        theView = inflater.inflate(R.layout.cases_referesher_fragment, container, false);
+
+        theView.findViewById(R.id.buttonNominativeInfo).setOnClickListener(this);
+        /*
+        theView.findViewById(R.id.buttonAccusative).setOnClickListener(this);
+        theView.findViewById(R.id.buttonGenitive).setOnClickListener(this);
+        theView.findViewById(R.id.buttonDative).setOnClickListener(this);
+        theView.findViewById(R.id.buttonInstrumental).setOnClickListener(this);
+        theView.findViewById(R.id.buttonPrepositional).setOnClickListener(this);
+        */
+
+        return theView;
     }
 
     public static CasesRefresherFragment newInstance ()
     {
         return new CasesRefresherFragment();
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch(view.getId()) {
+            case R.id.buttonNominativeInfo:
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setTitle(R.string.nominativeCaseTitle);
+                AlertDialog dialog = builder.create();
+                dialog.show();
+                break;
+        }
     }
 }
