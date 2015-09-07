fb3f3d43debf269aad2df065a9483d9b65bef49e
Victory <git@dfhu.org>
2015-09-07 14:37:17 -0400

    front of flash cards show pronoun, back shows case

--- app/src/main/java/org/dfhu/learnrussianpersonalpronouns/FlashCardsFragment.java
+++ app/src/main/java/org/dfhu/learnrussianpersonalpronouns/FlashCardsFragment.java
@@ -1,46 +1,43 @@
 package org.dfhu.learnrussianpersonalpronouns;
 
-import android.os.Bundle;
-import android.os.Handler;
+
 import android.support.annotation.Nullable;
+
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import org.dfhu.rulanguage.RuLanguage;
 import org.dfhu.rulanguage.RuPronoun;
-import org.w3c.dom.Text;
 
-public class FlashCardsFragment extends Fragment implements View.OnClickListener {
+
+public class FlashCardsFragment extends Fragment {
 
     private View theView;
-    private Button theButton;
     private static RuPronoun ruWord;
-    private TextView flashCardAnswer;
     private Boolean isFrontView = true;
-    private Handler mHandler = new Handler();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         theView = inflater.inflate(R.layout.flash_cards_fragment, container, false);
+        ruWord = (RuPronoun) RuLanguage.getRandomWord();
 
         if (savedInstanceState == null) {
             isFrontView = true;
             getChildFragmentManager()
                     .beginTransaction()
-                    .add(R.id.flashCardFragment, new CardFrontFragment())
+                    .add(R.id.flashCardFragment, CardFrontFragment.getInstance())
                     .commit();
         }
 
         final FrameLayout clicky = (FrameLayout) theView.findViewById(R.id.flashCardFragment);
-        clicky.findViewById(R.id.flashCardFront);
 
         clicky.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,9 +45,10 @@ public class FlashCardsFragment extends Fragment implements View.OnClickListener
                 if (isFrontView) {
                     flipCard();
                 } else {
+                    ruWord = (RuPronoun) RuLanguage.getRandomWord();
                     getChildFragmentManager()
                             .beginTransaction()
-                            .replace(R.id.flashCardFragment, new CardFrontFragment())
+                            .replace(R.id.flashCardFragment, CardFrontFragment.getInstance())
                             .commit();
                 }
                 isFrontView = !isFrontView;
@@ -67,17 +65,10 @@ public class FlashCardsFragment extends Fragment implements View.OnClickListener
                 .setCustomAnimations(
                         R.anim.card_flip_left_in, R.anim.card_flip_left_out,
                         R.anim.card_flip_right_in, R.anim.card_flip_right_out)
-                .replace(R.id.flashCardFragment, new CardBackFragment())
+                .replace(R.id.flashCardFragment, CardBackFragment.getInstances())
                 .addToBackStack(null)
                 .commit();
 
-    }
-    @Override
-    public void onClick(View view) {
-        String msg = "Case: " + ruWord.getCase();
-        flashCardAnswer.setText(msg);
-        flashCardAnswer.setVisibility(View.VISIBLE);
-        theButton.setVisibility(View.GONE);
     }
 
     public static FlashCardsFragment newInstance() {
@@ -85,18 +76,37 @@ public class FlashCardsFragment extends Fragment implements View.OnClickListener
     }
 
     static public class CardFrontFragment extends Fragment {
+
+        public CardFrontFragment () {}
+
+        public static CardFrontFragment getInstance() {
+            return new CardFrontFragment();
+        }
+
+
         @Nullable
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-            return inflater.inflate(R.layout.flash_cards_front, container, false);
+            LinearLayout frameLayout = (LinearLayout) inflater.inflate(R.layout.flash_cards_front, container, false);
+            TextView textView = (TextView)frameLayout.findViewById(R.id.flashCardFront);
+            textView.setText(ruWord.getWord());
+            return frameLayout;
         }
     }
 
     static public class CardBackFragment extends Fragment {
+
+        public static CardBackFragment getInstances () {
+            return new CardBackFragment();
+        }
+
         @Nullable
         @Override
         public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-            return inflater.inflate(R.layout.flash_cards_back, container, false);
+            LinearLayout frameLayout = (LinearLayout) inflater.inflate(R.layout.flash_cards_back, container, false);
+            TextView textView = (TextView)frameLayout.findViewById(R.id.flashCardBack);
+            textView.setText(ruWord.getCase());
+            return frameLayout;
         }
     }
 }
