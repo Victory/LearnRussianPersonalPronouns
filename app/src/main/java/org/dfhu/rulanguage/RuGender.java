5bc415588b01423d603a3f790dde080fdd82e74a
Victory <git@dfhu.org>
2015-09-28 19:31:08 -0400

    more words

--- app/src/main/java/org/dfhu/rulanguage/RuGender.java
+++ app/src/main/java/org/dfhu/rulanguage/RuGender.java
@@ -7,4 +7,5 @@ public class RuGender {
     public static final String MAS = "Masculine";
     public static final String FEM = "Feminine";
     public static final String NEU = "Neuter";
+    public static final String NA = "";
 }
--- app/src/main/java/org/dfhu/rulanguage/RuLanguage.java
+++ app/src/main/java/org/dfhu/rulanguage/RuLanguage.java
@@ -26,12 +26,26 @@ public class RuLanguage {
         }
 
         words = new ArrayList<>();
-        words.add(new RuPronoun("Я", RuCase.NOM, "1st", RuPronoun.SING, RuGender.MAS));
-        words.add(new RuPronoun("Меня", RuCase.ACU, "1st", RuPronoun.SING, RuGender.MAS));
+        words.add(new RuPronoun("Я", RuCase.NOM, "1st", RuPronoun.SING, RuGender.NA));
+        words.add(new RuPronoun("Меня", RuCase.ACU, "1st", RuPronoun.SING, RuGender.NA));
+        words.add(new RuPronoun("Меня", RuCase.GEN, "1st", RuPronoun.SING, RuGender.NA));
+        words.add(new RuPronoun("Мне", RuCase.DAT, "1st", RuPronoun.SING, RuGender.NA));
+        words.add(new RuPronoun("Мной", RuCase.INS, "1st", RuPronoun.SING, RuGender.NA));
+        words.add(new RuPronoun("Мне", RuCase.PRE, "1st", RuPronoun.SING, RuGender.NA));
+
+        words.add(new RuPronoun("Ты", RuCase.NOM, "2nd", RuPronoun.SING_POSS, RuGender.NA));
+        words.add(new RuPronoun("Тебя", RuCase.ACU, "2nd", RuPronoun.SING_POSS, RuGender.NA));
+        words.add(new RuPronoun("Тебя", RuCase.GEN, "2nd", RuPronoun.SING_POSS, RuGender.NA));
+        words.add(new RuPronoun("Тебе", RuCase.DAT, "2nd", RuPronoun.SING_POSS, RuGender.NA));
+        words.add(new RuPronoun("Тобой", RuCase.INS, "2nd", RuPronoun.SING_POSS, RuGender.NA));
+        words.add(new RuPronoun("Тебе", RuCase.PRE, "2nd", RuPronoun.SING_POSS, RuGender.NA));
+
+
         words.add(new RuPronoun("Моего", RuCase.GEN, "1st", RuPronoun.SING_POSS, RuGender.MAS));
         words.add(new RuPronoun("Моему", RuCase.DAT, "1st", RuPronoun.SING_POSS, RuGender.MAS));
         words.add(new RuPronoun("Моим", RuCase.INS, "1st", RuPronoun.SING_POSS, RuGender.MAS));
         words.add(new RuPronoun("Моём", RuCase.PRE, "1st", RuPronoun.SING_POSS, RuGender.MAS));
+
     }
 
 
